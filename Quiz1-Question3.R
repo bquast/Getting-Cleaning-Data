bbdcb61645d2ad85f748c4e09ccc6cf49a8f2c8a
Bastiaan Quast <bquast@gmail.com>
2014-05-07 12:01:13 +0200

    quiz 1 question 3, complete

--- Quiz1-Question3.R
+++ Quiz1-Question3.R
@@ -14,3 +14,13 @@ download.file(file.url, file.dest, method='curl' )
 
 # load the package to read xlsx files
 library(xlsx)
+
+# specify the rows and columns to import
+rowIndex <- 18:23
+colIndex <- 7:15
+
+# read the xlsx file
+dat <- read.xlsx('NGAP.xlsx', sheetIndex=1, rowIndex = rowIndex, colIndex = colIndex)
+
+# perform the required test
+sum(dat$Zip*dat$Ext,na.rm=T)
